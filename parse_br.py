e939e531814912095eabb146ce34ef3cd7dae548
Jason Maloney <jmaloney@linux.com>
2016-08-12 18:00:44 -0500

    Rework for new sheet format

--- parse_br.py
+++ parse_br.py
@@ -4,30 +4,19 @@ from __future__ import print_function
 import os
 import sys
 import csv
-import re
 import sqlite3
 
-pattern = r'([a-fA-F0-9xX]{8})'
-"""
-with open('att-br.csv', 'rb') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    col = {x:ind for ind, x in enumerate(next(csvreader))}
-    for row in csvreader:
-        matches = set(int(x[:6], 16) for x in re.findall(
-            pattern, row[col['Notes']]))
-        if len(matches):
-            print(matches, row[col['Notes']])
-"""
-
-
-
 gci_map = {
-        'B17 LTE PCI': ['0F', '10', '11', '12'],
-        'B4 LTE PCI': ['16', '17', '18', '19'],
-        'B2 LTE PCI': ['08', '09', '0A', '0B'],
-        #'B4 PCI 2': ['32', '33', '34', '35'],
+        'B17 PCI': ['0F', '10', '11', '12'],
+        'B4 PCI': ['16', '17', '18', '19'],
+        'B2 PCI': ['08', '09', '0A', '0B'],
+        'B4-2 PCI': ['32', '33', '34', '35'],
+        'B2-2 PCI': ['24', '25', '26', '27'],
         'B5 PCI': ['01', '02', '03', '04'],
+        'B30 PCI': ['95', '96', '97', '98'],
         }
+
+# CREATE TABLE sites_lte (_id INTEGER PRIMARY KEY, first_time NUMERIC, first_time_offset NUMERIC, last_time NUMERIC, last_time_offset NUMERIC, last_device_latitude NUMERIC, last_device_longitude NUMERIC, last_device_loc_accuracy NUMERIC, user_note TEXT, provider TEXT, plmn NUMERIC, gci TEXT, pci NUMERIC, tac NUMERIC, dl_chan NUMERIC, strongest_rsrp NUMERIC, strongest_latitude NUMERIC, strongest_longitude NUMERIC)
 
 if len(sys.argv) < 3:
     print('Usage: ' + sys.argv[0] + ' csvfile dbfile')
@@ -83,36 +72,29 @@ with open(sys.argv[1], 'rb') as csvfile:
         if 'CONFIRM' in row[cols['Notes']]:
             confirmed = False
             name = name + ' NEW'
-        processed = list()
-        for gci_col in ['B17 LTE GCI', 'B4 LTE GCI', 'B2 LTE GCI', 'B5 LTE GCI', 'Notes']:
-            if gci_col == 'Notes':
-                matches = set(int(x[:6], 16) for x in re.findall(
-                    pattern, row[cols['Notes']]))
-                if len(matches):
-                    # For now just use the first match
-                    gci = '{:06X}xx'.format(matches.pop())
-            else:
-                gci = row[cols[gci_col]]
-            if gci:
-                gci = gci[:6]
-                if gci in processed:
-                    continue
-                processed.append(gci)
+        for gci_col in ['LTE GCI-1', 'LTE GCI-2', 'LTE GCI-3']:
+            gci = row[cols[gci_col]]
+            if gci and '?' not in gci:
+                inserted = False
+                tac = row[cols['TAC']]
+                lat = row[cols['LAT']]
+                lon = row[cols['LONG']]
                 for pci_col in gci_map:
                     pcis = row[cols[pci_col]]
                     if pcis:
                         for idx, pci in enumerate(pcis.split(',')):
                             pci = pci.strip()
                             if pci != '?' and pci.isdigit():
-                                insert(row[cols['TAC']],
-                                        gci + gci_map[pci_col][idx],
+                                inserted = True
+                                insert(tac,
+                                        gci[:6] + gci_map[pci_col][idx],
                                         pci,
-                                        row[cols['LAT']],
-                                        row[cols['LONG']],
+                                        lat,
+                                        lon,
                                         name,
                                         confirmed)
-
-
+                if not inserted:
+                    insert(tac, gci, None, lat, lon, name, confirmed)
 
 conn.commit()
 conn.close()
